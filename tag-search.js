3fe79035008e117e4b248ca29b95f9d39f7c7504
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-09-19 19:47:03 +0200

    Add files via upload

--- tag-search.js
+++ tag-search.js
@@ -5,19 +5,22 @@ app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const homepath = app.pathTo("home folder");
 const vault_path = $.getenv("vault_path").replace(/^~/, homepath);
-const tagsJSON = vault_path + "/.obsidian/plugins/launcher-bridge/tags.json";
+const tagsJSON = vault_path + "/.obsidian/plugins/metadata-extractor/tags.json";
 
 var tags_array = app.doShellScript(
-   'cat "' + tagsJSON + '" | grep ' + "'" + '"' + 'tag' + '"' + "'"
-   + "| cut -d '" + '"' + "' -f 4"
-).split("\r");
+   'cat "' + tagsJSON + '" | grep -A1 ' + "'" + '"tag"' + "'"
+    + ' | cut -d: -f2'
+).split("--\r");
 
 
 let jsonArray = [];
-tags_array.forEach(tagName => {
+tags_array.forEach(tagData => {
+	tagName = tagData.split("\r")[0].split('"')[1];
+	tagCount = tagData.split("\r")[1].slice(1,-1);
 
 	jsonArray.push({
 		'title': "#" + tagName,
+		'subtitle': tagCount,
 		'match': tagName,
 		'arg': tagName,
 	});
