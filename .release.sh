57364a54ef834757547d3148fe2f1db0322957b9
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-11-22 17:41:09 +0100

    release automation now fully written in shell without any dependencies

--- .release.sh
+++ .release.sh
@@ -1,13 +1,11 @@
 #!/bin/zsh
 
-# Alfred workflow release
+# ALFRED WORKFLOW RELEASE
 # -----------------------
 
-# safety net
-if [[ !-f ".release-plist-edit.js" ]] ; then
-	echo ".release-plist-edit.js missing. Aborting."
-	return
-fi
+# -----------------------
+# new version number
+# -----------------------
 
 # prompt for version
 lastVersion=$(plutil -extract version xml1 -o - info.plist | sed -n 4p | cut -d">" -f2 | cut -d"<" -f1)
@@ -21,11 +19,26 @@ osascript -e 'tell application "Alfred Preferences" to if it is running then qui
 # insert new version number
 plutil -replace version -string "$nextVersion" info.plist
 
-# bkp
+# -----------------------
+# clean info.plist
+# -----------------------
+
+# bkp info.plist
 cp -v info.plist info-original.plist
 
-# create clean info.plist for release
-osascript -l JavaScript ".release-plist-edit.js"
+# list of all variables to be excluded
+excludedVars=$(plutil -extract variablesdontexport json -o - info.plist | tr -d "[]\"" | tr "," "\n")
+
+# remove from info.plist
+echo "$excludedVars" | tr "\n" "\0" | xargs -0 -I § plutil -replace variables.§ -string "" info.plist
+
+# report exluded number
+exclusionNo=$(echo $excludedVars | wc -l | tr -d " ")
+echo "Removed $exclusionNo variables flagged as 'no export' removed from 'info.plist'."
+
+# -----------------------
+# compile .alfredworkflow
+# -----------------------
 
 # remove any potentially existing leftover workflow file
 mv -v *.alfredworkflow ~/.trash
@@ -40,9 +53,13 @@ echo "new $workflowName.alfredworkflow file created."
 mv -v info.plist ~/.trash
 mv -v info-original.plist info.plist
 
-echo ""
 
-# # push to remote
+# --------------------
+# git push
+# --------------------
+
+echo ""
+# push to remote
 git add -A
 git commit -m "release $nextVersion"
 git push
