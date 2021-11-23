bb73932ffbcd0b8cf7066fbd19f4571eede82207
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-11-23 21:32:06 +0100

    linebreak added

--- .release.sh
+++ .release.sh
@@ -12,6 +12,7 @@ lastVersion=$(plutil -extract version xml1 -o - info.plist | sed -n 4p | cut -d"
 echo "Last Version: $lastVersion"
 echo -n "Next Version: "
 read nextVersion
+echo ""
 
 # Close Alfred Prefs to avoid conflicts
 osascript -e 'tell application "Alfred Preferences" to if it is running then quit'
