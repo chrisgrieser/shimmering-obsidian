51f79a81aaaab3ea81ded5e43c4406835c186210
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-04 16:33:03 +0100

    + fixed display of headings in documentation search

--- scripts/re-index-documentation.js
+++ scripts/re-index-documentation.js
@@ -3,45 +3,60 @@ ObjC.import("stdlib");
 app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
-function onlineJSON (url){
+function onlineJSON (url) {
 	return JSON.parse (app.doShellScript("curl -s \"" + url + "\""));
 }
 
-function alfredMatcher (str){
-	return str.replace (/[-()_.]/g, " ") + " " + str + " ";
+function alfredMatcher (str) {
+	return " " + str.replace (/[-()_#+.]/g, " ") + " " + str + " ";
 }
 
 const jsonArray = [];
 const officialDocsURL = "https://help.obsidian.md/";
 const officialDocsJSON = onlineJSON("https://api.github.com/repos/obsidianmd/obsidian-docs/git/trees/master?recursive=1");
+const rawGitHubURL = "https://raw.githubusercontent.com/obsidianmd/obsidian-docs/master/";
 const communityDocsURL = "https://publish.obsidian.md/hub/";
 const communityDocsJSON = onlineJSON("https://api.github.com/repos/obsidian-community/obsidian-hub/git/trees/main?recursive=1");
-const rawGitHubURL = "https://raw.githubusercontent.com/obsidianmd/obsidian-docs/master/";
 
 
 // > OFFICIAL DOCS
 // --------------------------------
-const officialDocs =
-	officialDocsJSON
-		.tree
-		.filter ( item =>
-			item.path.slice(-3) === ".md" &&
+const officialDocs =	officialDocsJSON
+	.tree
+	.filter ( item =>
+		item.path.slice(-3) === ".md" &&
 		item.path.slice(0, 3) === "en/" &&
 		item.path.slice(0, 9) !== "en/.trash"
-		);
+	);
 
-// get the headings
+officialDocs.forEach(item => {
+	const area = item.path.split("/").slice(1, -1).join("/");
+	const url = officialDocsURL + item.path.slice(3, -3).replaceAll (" ", "+");
+	const title = item.path.split("/").pop().slice(0, -3);
+
+	jsonArray.push({
+		"title": title,
+		"match": alfredMatcher(title),
+		"subtitle": area,
+		"uid": url,
+		"arg": url,
+	});
+});
+
+// > HEADINGS of Official Docs
+// --------------------------------
+
 const documentationHeaders = [];
 officialDocs.forEach(doc => {
 	const docURL = rawGitHubURL + encodeURI(doc.path);
-	const docHeaders = app.doShellScript("curl -s '" + docURL + "' | grep -E '^#' | cut -d ' ' -f 2-").split("\r");
+	const docHeaders = app.doShellScript("curl -sL '" + docURL + "' | grep -E '^#' | cut -d ' ' -f 2-").split("\r");
 
 	// add header to search hits
-	if (docHeaders[0] !== ""){
+	if (docHeaders[0] !== "")
 		docHeaders.forEach(headerName => {
 			documentationHeaders.push (doc.path + "#" + headerName);
 		});
-	}
+
 });
 
 documentationHeaders.forEach(header => {
@@ -54,37 +69,8 @@ documentationHeaders.forEach(header => {
 		"subtitle": area,
 		"uid": url,
 		"match": alfredMatcher(header),
-		"arg": url
+		"arg": url,
 	});
-});
-
-
-officialDocs.forEach(item => {
-	const area = item.path.split("/").slice(1, -1).join("/");
-	const url = officialDocsURL + item.path.slice(3, -3).replaceAll (" ", "+");
-	const title = item.path.split("/").pop().slice(0, -3);
-
-	jsonArray.push({
-		"title": title,
-		"match": alfredMatcher(title),
-		"subtitle": area,
-		"uid": url,
-		"arg": url
-	});
-});
-
-
-// index and start-here
-jsonArray.push({
-	"title": "Index",
-	"arg": "https://help.obsidian.md/Index",
-	"uid": "1",
-});
-
-jsonArray.push({
-	"title": "Start here",
-	"arg": "https://help.obsidian.md/Start+here",
-	"uid": "2",
 });
 
 
@@ -103,7 +89,7 @@ communityDocs.forEach(item => {
 	jsonArray.push({
 		"title": title,
 		"match": alfredMatcher(title),
-		"icon": {"path" : "icons/community-vault.png"},
+		"icon": { "path" : "icons/community-vault.png" },
 		"subtitle": area,
 		"uid": url,
 		"arg": url
