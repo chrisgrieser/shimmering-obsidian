0ee0dd69a3543b244d87bd9b563a9f9beb54c435
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-07 17:03:35 +0100

    svg

--- scripts/svg-to-data-uri.sh
+++ scripts/svg-to-data-uri.sh
@@ -9,7 +9,7 @@ pbpaste > temp.svg
 svgDataURI=$(mini-svg-data-uri temp.svg | sed -e "s/width='1em'/width='$svg_size'/" | sed -e "s/height='1em'/height='$svg_size'/" | sed -e "s/xmlns:.*role='img' //")
 rm temp.svg
 
-cssReady="svg.PLACEHOLDER > path {\n\tdisplay: none;\n}\n\nsvg.PLACEHOLDER {\n\tbackground-color: currentColor;\n\t-webkit-mask-image: url(\"$svgDataURI\");\n}"
+cssReady="svg.PLACEHOLDER > path { display: none }\n\nsvg.PLACEHOLDER {\n\tbackground-color: currentColor;\n\t-webkit-mask-image: url(\"$svgDataURI\");\n}"
 
 echo "$cssReady" | pbcopy
 
