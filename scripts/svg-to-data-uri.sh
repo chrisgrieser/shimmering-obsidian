2077f2a2932a42d6602aff4ff0930f9751ad3d1a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-07 16:31:56 +0100

    fix for osvg

--- scripts/svg-to-data-uri.sh
+++ scripts/svg-to-data-uri.sh
@@ -6,7 +6,7 @@ if [[ "$svg_size" == "" ]] ; then
 fi
 
 pbpaste > temp.svg
-svgDataURI=$(mini-svg-data-uri temp.svg | sed -e "s/width='1em'/width='$svg_size'/" | sed -e "s/height='1em'/height='$svg_size'/" | sed -e "s/xmlns.*role='img' //")
+svgDataURI=$(mini-svg-data-uri temp.svg | sed -e "s/width='1em'/width='$svg_size'/" | sed -e "s/height='1em'/height='$svg_size'/" | sed -e "s/xmlns:.*role='img' //")
 rm temp.svg
 
 cssReady="svg.PLACEHOLDER > path {\n\tdisplay: none;\n}\n\nsvg.PLACEHOLDER {\n\tbackground-color: currentColor;\n\t-webkit-mask-image: url(\"$svgDataURI\");\n}"
