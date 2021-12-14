b2b3810c2c2b1e5487da2e7cda33fe63869603de
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-14 11:08:06 +0100

    address #39

--- scripts/open-note.js
+++ scripts/open-note.js
@@ -6,21 +6,18 @@ function run (argv) {
 	// import variables
 	const relativePath = argv.join("").split("#")[0];
 	const heading = argv.join("").split("#")[1];
-	const vaultName = $.getenv("vault_path").split("/").pop();
+	const vaultName = $.getenv("vault_name_ENC");
 
 	let urlScheme = "obsidian://";
-	if (heading === undefined) {
-		urlScheme +=
-			"open?vault=" + encodeURIComponent (vaultName) +
-			"&file=" + encodeURIComponent(relativePath);
-	} else {
-		urlScheme +=
-			"advanced-uri?vault=" + encodeURIComponent (vaultName) +
-			"&filepath=" + encodeURIComponent(relativePath) +
-			"&heading= " + encodeURIComponent(heading);
-	}
+	if (!heading) urlScheme +=
+		"open?vault=" + vaultName +
+		"&file=" + encodeURIComponent(relativePath);
+	if (heading) urlScheme +=
+		"advanced-uri?vault=" + vaultName +
+		"&filepath=" + encodeURIComponent(relativePath) +
+		"&heading= " + encodeURIComponent(heading);
 
-	app = Application.currentApplication();
+	const app = Application.currentApplication();
 	app.includeStandardAdditions = true;
 	app.openLocation (urlScheme);
 }
