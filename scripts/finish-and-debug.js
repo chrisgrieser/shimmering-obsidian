c2e59ca5264c7a08be8dde183f76c7f3856aed9a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-12-23 17:38:03 +0100

    fixes for debugging function

--- scripts/finish-and-debug.js
+++ scripts/finish-and-debug.js
@@ -26,52 +26,58 @@ function run(argv) {
 		else console.log (str);
 	}
 
+	function debugLog () {
+		const appTempPath = app.pathTo("home folder") + "/Library/Application Support/obsidian/";
+		const obsiVer = app.doShellScript("cd '" + appTempPath + "'; ls *.asar | grep -Eo '(\\d|\\.)*'").slice (0, -1);
+		const macVer = app.doShellScript("sw_vers -productVersion");
+		const advancedUriVer = JSON.parse(readFile(vaultPath + "/.obsidian/plugins/obsidian-advanced-uri/manifest.json")).version;
+		const metadataExVer = JSON.parse(readFile(vaultPath + "/.obsidian/plugins/metadata-extractor/manifest.json")).version;
+
+		const advancedUriVerOnline = onlineJSON("https://github.com/Vinzent03/obsidian-advanced-uri/releases/latest/download/manifest.json")
+			.version;
+		const metadataExVerOnline = onlineJSON("https://github.com/kometenstaub/metadata-extractor/releases/latest/download/manifest.json")
+			.version;
+		const obsiVerOnline = onlineJSON("https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json")
+			.latestVersion;
+		const obsiVerBetaOnline = onlineJSON("https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json")
+			.beta.latestVersion;
+		const workflowVerOnline = onlineJSON("https://api.github.com/repos/chrisgrieser/shimmering-obsidian/tags")[0]
+			.name;
+
+		const numberOfJSONS = app.doShellScript("ls '" + vaultPath + "/.obsidian/plugins/metadata-extractor/' | grep \".json\" | grep -v \"manifest\" | grep -v \"^data\" | wc -l | tr -d \" \"");
+
+		log("_");
+		log("-------------------------------");
+		log("Metadata JSONs: " + numberOfJSONS + "/3");
+		if (numberOfJSONS < 3) log("Metadata not found. Please run `osetup` and retry.")
+		log("-------------------------------");
+		log("INSTALLED VERSION");
+		log("macOS: " + macVer);
+		log("Obsidian: " + obsiVer);
+		log("Alfred: " + $.getenv("alfred_version"));
+		log("Workflow: " + $.getenv("alfred_workflow_version"));
+		log("Advanced URI Extractor: " + advancedUriVer);
+		log("Metadata Extractor: " + metadataExVer);
+		log("-------------------------------");
+		log("LATEST VERSION");
+		log("Obsidian: " + obsiVerOnline);
+		log("Obsidian (Insider): " + obsiVerBetaOnline);
+		log("Workflow: " + workflowVerOnline);
+		log("Advanced URI Extractor: " + advancedUriVerOnline);
+		log("Metadata Extractor: " + metadataExVerOnline);
+		log("-------------------------------");
+	}
+
 	// remove config
 	Application("com.runningwithcrayons.Alfred").removeConfiguration("ObRunning", { inWorkflow: $.getenv("alfred_workflow_bundleid") } );
 
-	// stop when no debugging required https://www.alfredapp.com/help/workflows/script-environment-variables/
-	if ($.getenv("alfred_debug") !== "1" && !odebug) return;
-
 	// log Version info to debugging log
-	const appTempPath = app.pathTo("home folder") + "/Library/Application Support/obsidian/";
-	const obsiVer = app.doShellScript("cd '" + appTempPath + "'; ls *.asar | grep -Eo '(\\d|\\.)*'").slice (0, -1);
-	const macVer = app.doShellScript("sw_vers -productVersion");
-	const advancedUriVer = JSON.parse(readFile(vaultPath + "/.obsidian/plugins/obsidian-advanced-uri/manifest.json")).version;
-	const metadataExVer = JSON.parse(readFile(vaultPath + "/.obsidian/plugins/metadata-extractor/manifest.json")).version;
+	// stop when no debugging required https://www.alfredapp.com/help/workflows/script-environment-variables/
+	if (odebug) {
+		debugLog();
+		return output;
+	}
+	if ($.getenv("alfred_debug") === "1") debugLog();
 
-	const advancedUriVerOnline = onlineJSON("https://github.com/kometenstaub/metadata-extractor/releases/latest/download/manifest.json")
-		.version;
-	const metadataExVerOnline = onlineJSON("https://github.com/Vinzent03/obsidian-advanced-uri/releases/latest/download/manifest.json")
-		.version;
-	const obsiVerOnline = onlineJSON("https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json")
-		.latestVersion;
-	const obsiVerBetaOnline = onlineJSON("https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/desktop-releases.json")
-		.beta.latestVersion;
-	const workflowVerOnline = onlineJSON("https://api.github.com/repos/chrisgrieser/shimmering-obsidian/tags")[0]
-		.name;
-
-	const numberOfJSONS = app.doShellScript("ls '" + vaultPath + "/.obsidian/plugins/metadata-extractor/' | grep \".json\" | grep -v \"manifest\" | grep -v \"^data\" | wc -l | tr -d \" \"");
-
-	log("_");
-	log("-------------------------------");
-	log("INSTALLED VERSION");
-	log("macOS: " + macVer);
-	log("Obsidian: " + obsiVer);
-	log("Alfred: " + $.getenv("alfred_version"));
-	log("Workflow: " + $.getenv("alfred_workflow_version"));
-	log("Advanced URI Extractor: " + advancedUriVer);
-	log("Metadata Extractor: " + metadataExVer);
-	log("-------------------------------");
-	log("Metadata JSONs: " + numberOfJSONS + "/3");
-	log("-------------------------------");
-	log("LATEST VERSION");
-	log("Obsidian: " + obsiVerOnline);
-	log("Obsidian (Insider): " + obsiVerBetaOnline);
-	log("Workflow: " + workflowVerOnline);
-	log("Advanced URI Extractor: " + advancedUriVerOnline);
-	log("Metadata Extractor: " + metadataExVerOnline);
-	log("-------------------------------");
-
-	if (output) return output;
 }
 
