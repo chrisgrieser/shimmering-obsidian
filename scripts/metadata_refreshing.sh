67704f4c408fb111586c98dc084b24f1a5e2de58
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-01-22 17:25:27 +0100

    added more delays to ensure proper writing of metadata

--- scripts/metadata_refreshing.sh
+++ scripts/metadata_refreshing.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
+open -a "Obsidian"
+sleep 1
 open "obsidian://advanced-uri?vault=""$vault_name_ENC""&commandid=metadata-extractor%253Awrite-metadata-json"
+sleep 0.5
 open "obsidian://advanced-uri?vault=""$vault_name_ENC""&commandid=metadata-extractor%253Awrite-tags-json"
+sleep 0.5
 open "obsidian://advanced-uri?vault=""$vault_name_ENC""&commandid=metadata-extractor%253Awrite-allExceptMd-json"
