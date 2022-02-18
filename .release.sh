a887330ecda33cc0feea52d03270371c957d2240
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-02-18 02:57:27 +0100

    added markdown-link-check

--- .release.sh
+++ .release.sh
@@ -11,7 +11,7 @@
 lastVersion=$(plutil -extract version xml1 -o - info.plist | sed -n 4p | cut -d">" -f2 | cut -d"<" -f1)
 echo "Last Version: $lastVersion"
 echo -n "Next Version: "
-read nextVersion
+read -r nextVersion
 echo ""
 
 # Close Alfred Prefs to avoid conflicts
@@ -21,9 +21,11 @@ osascript -e 'tell application "Alfred Preferences" to if it is running then qui
 plutil -replace version -string "$nextVersion" info.plist
 
 # Lint
-cd "$(dirname "$0")"
-markdownlint --fix *.md
+cd "$(dirname "$0")" || exit
+markdownlint --fix ./README.md
 markdownlint --fix docs/*.md
+markmarkdown-link-check -q ./README.md
+markmarkdown-link-check -q docs/*.md
 
 # -----------------------
 # clean info.plist
@@ -39,7 +41,7 @@ excludedVars=$(plutil -extract variablesdontexport json -o - info.plist | tr -d 
 echo "$excludedVars" | tr "\n" "\0" | xargs -0 -I § plutil -replace variables.§ -string "" info.plist
 
 # report excluded number
-exclusionNo=$(echo $excludedVars | wc -l | tr -d " ")
+exclusionNo=$(echo "$excludedVars" | wc -l | tr -d " ")
 echo "Removed $exclusionNo variables flagged as 'no export' removed from 'info.plist'."
 
 # -----------------------
@@ -47,11 +49,11 @@ echo "Removed $exclusionNo variables flagged as 'no export' removed from 'info.p
 # -----------------------
 
 # remove any potentially existing leftover workflow file
-mv -fv *.alfredworkflow ~/.trash
+mv -fv /.*.alfredworkflow ~/.trash
 
 # zip
 workflowName=$(plutil -extract name xml1 -o - info.plist | sed -n 4p | cut -d">" -f2 | cut -d"<" -f1)
-workflowName=$(echo $workflowName | tr " " "-")
+workflowName=$(echo "$workflowName" | tr " " "-")
 zip --quiet -r "$workflowName.alfredworkflow" . -x ".git/*" ".github/*" "documentation/*" ".release.sh" ".gitignore" "info-original.plist" ".release-plist-edit.js" "README.md" ".DS_Store" "*.alfredworkflow"
 echo "new $workflowName.alfredworkflow file created."
 
@@ -65,7 +67,7 @@ echo ""
 # -----------------------
 
 # update changelog
-echo "- "$(date +"%Y-%m-%d")"	release $nextVersion" > ./Changelog.md
+echo "- ""$(date +"%Y-%m-%d")""	release $nextVersion" > ./Changelog.md
 git log --pretty=format:"- %ad%x09%s" --date=short | grep -Ev "minor$" | grep -Ev "patch$" | grep -Ev "typos?$" | grep -v "refactoring" | grep -v "Add files via upload" | grep -Ev "\tDelete" | grep -Ev "\tUpdate.*\.md" | sed -E "s/\t\+ /\t/g" >> ./Changelog.md
 
 git add -A
