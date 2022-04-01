556edf548d8be835ac1b453cc2f060a4a7f030eb
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-01 22:30:01 +0200

    patch

--- scripts/metadata_refreshing.sh
+++ scripts/metadata_refreshing.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
+# shellcheck disable=SC2154
 open -a "Obsidian"
 sleep 1
-open "obsidian://advanced-uri?vault=""$vault_name_ENC""&commandid=metadata-extractor%253Awrite-metadata-json"
+open "obsidian://advanced-uri?vault=$vault_name_ENC&commandid=metadata-extractor%253Awrite-metadata-json"
 sleep 0.5
-open "obsidian://advanced-uri?vault=""$vault_name_ENC""&commandid=metadata-extractor%253Awrite-tags-json"
+open "obsidian://advanced-uri?vault=$vault_name_ENC&commandid=metadata-extractor%253Awrite-tags-json"
 sleep 0.5
-open "obsidian://advanced-uri?vault=""$vault_name_ENC""&commandid=metadata-extractor%253Awrite-allExceptMd-json"
+open "obsidian://advanced-uri?vault=$vault_name_ENC&commandid=metadata-extractor%253Awrite-allExceptMd-json"
