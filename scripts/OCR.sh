fa8dd4fd8c1a33b3894cb874344663ae0ce8c88b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-04-19 21:35:27 +0200

    patch

--- scripts/OCR.sh
+++ scripts/OCR.sh
@@ -2,12 +2,12 @@
 export PATH=/usr/local/bin/:/opt/homebrew/bin/:$PATH
 
 # OCR
-temp_image="$alfred_workflow_cache""/temp_ocr_snapshot.png"
+temp_image="$alfred_workflow_cache/temp_ocr_snapshot.png"
 mkdir -p "$alfred_workflow_cache"
 screencapture -i "$temp_image"
 ocr_text=$(tesseract "$temp_image" stdout -l $ocr_languages 2>&1)
 ocr_prefix="$*" # has to be input from Alfred for date variable resolution
-ocr_text="$ocr_prefix""\n\n""$ocr_text"
+ocr_text="$ocr_prefix\n\n$ocr_text"
 
 # if using multiple monitors
 ocr_text=$(echo "$ocr_text" | egrep -v "Warning: Invalid resolution 0 dpi." | egrep -v "Estimating resolution as")
