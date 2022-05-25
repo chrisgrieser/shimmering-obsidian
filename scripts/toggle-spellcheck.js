d754610a5338ad20e2b657b767d6b1ff20ab0f2b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-25 10:28:46 +0200

    fix spellcheck toggling when changing workspaces

--- scripts/toggle-spellcheck.js
+++ scripts/toggle-spellcheck.js
@@ -16,23 +16,22 @@ function run (argv) {
 
 	// get target spellcheck status
 	const urlscheme = argv.join("");
-	const workspaceName = decodeURIComponent(urlscheme.split("=")[1]);
+	const workspaceName = decodeURIComponent(urlscheme.split("=")[2]);
+
 	const workspaceToSpellcheck = $.getenv("workspace_to_spellcheck");
 
-	// abort and do nothing when workspaces are managed, spellcheck toggled
-	// or no spellcheck-workspace defined
-	if (urlscheme === "obsidian://advanced-uri?commandid=workspaces%253Aopen-modal") return;
-	if (urlscheme === "obsidian://advanced-uri?commandid=editor%253Atoggle-spellcheck") return;
-	if (urlscheme === "obsidian://advanced-uri?saveworkspace=true") return;
+	// abort and do nothing when workspaces are managed, spellcheck toggled, or no spellcheck-workspace defined
+	if (urlscheme.endsWith("workspaces%253Aopen-modal")) return;
+	if (urlscheme.endsWith("editor%253Atoggle-spellcheck")) return;
+	if (urlscheme.endsWith("saveworkspace=true")) return;
 	if (workspaceToSpellcheck === "") return;
 
 	// get current spellcheck status
 	const vaultPath = $.getenv("vault_path").replace(/^~/, app.pathTo("home folder"));
 	const turnSpellCheckOn = workspaceName === workspaceToSpellcheck;
-	const currentSpellCheck = JSON.parse(readFile(vaultPath + "/.obsidian/app.json")).spellcheck === "true";
+	const currentSpellCheck = JSON.parse(readFile(vaultPath + "/.obsidian/app.json")).spellcheck;
 
 	// toggle if change is needed
-	if (turnSpellCheckOn !== currentSpellCheck)
-		app.openLocation("obsidian://advanced-uri?vault=" + $.getenv("vault_name_ENC") + "&commandid=editor%253Atoggle-spellcheck");
+	if (turnSpellCheckOn !== currentSpellCheck) app.openLocation("obsidian://advanced-uri?vault=" + $.getenv("vault_name_ENC") + "&commandid=editor%253Atoggle-spellcheck");
 
 }
