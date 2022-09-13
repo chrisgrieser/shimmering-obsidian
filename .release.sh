b8e7d0cc7574120b988848a95fabc7f1921f63c5
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-09-13 15:30:21 +0200

    update release automation for Alfred 5

--- .release.sh
+++ .release.sh
@@ -19,7 +19,6 @@ if [[ -z "$nextVersion" ]]; then
 else
 	echo "$nextVersion"
 fi
-echo ""
 
 # Close Alfred Prefs to avoid conflicts
 osascript -e 'tell application "Alfred Preferences" to if it is running then quit'
@@ -29,7 +28,6 @@ plutil -replace version -string "$nextVersion" info.plist
 
 # Lint
 cd "$(dirname "$0")" || exit 1
-# TODO: lint some older files
 eslint . --fix
 
 echo ""
@@ -45,7 +43,7 @@ cp -v info.plist info-original.plist
 excludedVars=$(plutil -extract variablesdontexport json -o - info.plist | tr -d "[]\"" | tr "," "\n")
 
 # remove from info.plist
-echo "$excludedVars" | tr "\n" "\0" | xargs -0 -I § plutil -replace variables.§ -string "" info.plist
+echo "$excludedVars" | tr "\n" "\0" | xargs -0 -I {} plutil -replace variables.{} -string "" info.plist
 
 # report excluded number
 exclusionNo=$(echo "$excludedVars" | wc -l | tr -d " ")
@@ -61,7 +59,7 @@ rm -fv ./*.alfredworkflow
 # zip
 workflowName=$(plutil -extract name xml1 -o - info.plist | sed -n 4p | cut -d">" -f2 | cut -d"<" -f1)
 workflowName=$(echo "$workflowName" | tr " " "-")
-zip --quiet -r "$workflowName.alfredworkflow" . -x ".git/*" ".github/*" "documentation/*" ".release.sh" ".gitignore" "info-original.plist" ".release-plist-edit.js" "README.md" ".DS_Store" "*.alfredworkflow"
+zip --quiet -r "$workflowName.alfredworkflow" . -x ".*" "promo*.png" "doc*/*" ".gitignore" "info-original.plist" "prefs.plist" "README.md" "*.alfredworkflow"
 echo "new $workflowName.alfredworkflow file created."
 
 # restore original
--- README.md
+++ README.md
@@ -173,10 +173,6 @@ In my day job, I am a sociologist studying the social mechanisms underlying the 
 ### Donate
 <a href='https://ko-fi.com/Y8Y86SQ91' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
 
-If you feel very generous, you may also buy me something from my Amazon wish list. But please donate something to developers who still go to college, before you consider buying me an item from my wish list! 😊
-
-[Amazon wish list](https://www.amazon.de/hz/wishlist/ls/2C7RIOJPN3K5F?ref_=wl_share)
-
 ---
 
 [⬆️ Go Back to Top](#Table-of-Contents)
