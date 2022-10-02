81f33fc15e31aad8b9dbf7b972645660d29facb5
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-10-02 12:46:11 +0200

    no more notice when opening note

--- scripts/open-note.js
+++ scripts/open-note.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env osascript -l JavaScript
-
 function run (argv) {
 	ObjC.import("stdlib");
+
+	const obsiRunningAlready = Application("Obsidian").running();
 
 	// import variables
 	const input = argv.join("").trim();
@@ -9,26 +10,17 @@ function run (argv) {
 	const heading = input.split("#")[1];
 	const vaultNameENC = $.getenv("vault_name_ENC");
 
-	let urlScheme = "obsidian://";
-	if (heading) {
-		urlScheme +=
-			"advanced-uri?vault=" + vaultNameENC +
-			"&filepath=" + encodeURIComponent(relativePath) +
-			"&heading=" + encodeURIComponent(heading);
-	} else {
-		urlScheme +=
-			"open?vault=" + vaultNameENC +
-			"&file=" + encodeURIComponent(relativePath);
-	}
+	let urlScheme = `obsidian://advanced-uri?vault=${vaultNameENC}&filepath=`
+		+ encodeURIComponent(relativePath);
+	if (heading) urlScheme += "&heading=" + encodeURIComponent(heading);
 
 	const app = Application.currentApplication();
 	app.includeStandardAdditions = true;
 	app.openLocation (urlScheme);
 
 	// press 'Esc' to leave settings menu
-	Application("System Events").keyCode(53); // eslint-disable-line no-magic-numbers
+	if (obsiRunningAlready) Application("System Events").keyCode(53); // eslint-disable-line no-magic-numbers
 
-	// for debugging
 	if ($.getenv("alfred_debug") === "1") {
 		console.log("Encoded Vault Name: " + vaultNameENC);
 		console.log("Relative Path: " + relativePath);
