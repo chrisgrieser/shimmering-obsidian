7b3a9dbe15c5f4206e768b3583783715aa5754e6
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-11-03 16:45:44 +0100

    use `on` with cmd+return to create in new tab

--- scripts/new-note.js
+++ scripts/new-note.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env osascript -l JavaScript
 
-function run (argv) {
+function run(argv) {
 	ObjC.import("stdlib");
 	ObjC.import("Foundation");
 	const app = Application.currentApplication();
 	app.includeStandardAdditions = true;
-	function readFile (path, encoding) {
+
+	function readFile(path, encoding) {
 		if (!encoding) encoding = $.NSUTF8StringEncoding;
 		const fm = $.NSFileManager.defaultManager;
 		const data = fm.contentsAtPath(path);
@@ -13,28 +14,49 @@ function run (argv) {
 		return ObjC.unwrap(str);
 	}
 
-	// Selected text, when hotkey was used
-	let selectedText;
+	function setEnvVar(envVar, newValue) {
+		Application("com.runningwithcrayons.Alfred")
+			.setConfiguration(envVar, {
+				toValue: newValue,
+				inWorkflow: $.getenv("alfred_workflow_bundleid"),
+				exportable: false
+			});
+	}
+
+	//───────────────────────────────────────────────────────────────────────────
+
+	const vaultNameEnc = $.getenv("vault_name_ENC");
+
+	let selectedText; // when hotkey was used
 	try {
-		selectedText= $.getenv("selected_text");
+		selectedText = $.getenv("selected_text");
 	} catch (error) {
 		selectedText = "";
 	}
 
+	let createInNewTab;
+	try {
+		createInNewTab = $.getenv("create_in_new_tab") === "true";
+	} catch (error) {
+		createInNewTab = false;
+	}
+	if (createInNewTab) app.openLocation(`obsidian://advanced-uri?vault=${vaultNameEnc}&commandid=workspace%253Anew-tab`);
+
 	let fileName = argv.join("");
 	if (!fileName) fileName = "Untitled";
-	(fileName = fileName
-		.replace (/[\\/:]/g, "")) // remove illegal charcters
-		.charAt(0).toUpperCase() + fileName.slice(1); // eslint-disable-line newline-per-chained-call
+	fileName = fileName.replace(/[\\/:]/g, ""); // remove illegal charcters
+	fileName = fileName.charAt(0).toUpperCase() + fileName.slice(1); // eslint-disable-line newline-per-chained-call
+
+	//───────────────────────────────────────────────────────────────────────────
 
 	const newNotePath = ($.getenv("new_note_location") + "/" + fileName)
-		.replaceAll ("//", "/");
+		.replaceAll("//", "/");
 
 	let URI = "obsidian://advanced-uri?" +
-		"vault=" + $.getenv("vault_name_ENC") +
+		"vault=" + vaultNameEnc +
 		"&filepath=" + encodeURIComponent(newNotePath) +
 		"&mode=new" +
-		"&line=999999999999999";
+		"&line=999999999999999"; // = append at end of file
 
 	// Content
 	let newNoteContent = "";
@@ -52,4 +74,8 @@ function run (argv) {
 	URI += "&data=" + encodeURIComponent(newNoteContent);
 	console.log("URI: " + URI);
 	app.openLocation(URI);
+
+	// reset
+	setEnvVar("create_in_new_tab", "");
+	setEnvVar("selected_text", "");
 }
