d70b4afb35961812f5ba9451f586c0831b153132
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-11-14 21:13:36 +0100

    `ocss` now only opens snippets

--- scripts/search_css_snippet.js
+++ scripts/search_css_snippet.js
@@ -6,41 +6,18 @@ app.includeStandardAdditions = true;
 const homepath = app.pathTo("home folder");
 const vaultPath = $.getenv("vault_path").replace(/^~/, homepath);
 const snippetPath = vaultPath + "/.obsidian/snippets/";
-const themePath = vaultPath + "/.obsidian/themes/";
-const currentTheme = app.doShellScript(`cat "${vaultPath}/.obsidian/appearance.json" | grep "cssTheme" | head -n 1 | cut -d '"' -f 4`);
 
 // Input
-const snippetArr = app.doShellScript("find '" + snippetPath + "' -name '*.css' ").split("\r");
-const themeArr = app.doShellScript("find '" + themePath + "' -name '*.css' ").split("\r");
-
-// JSON Construction
-const jsonArray = [];
-themeArr.forEach(themeFilePath => {
-	const filename = themeFilePath.replace(/.*\/(.*)\..+/, "$1");
-	let currentIcon = "";
-	let subtitlePrefix = "";
-	if (currentTheme === filename) {
-		currentIcon = "✅ ";
-		subtitlePrefix = "current ";
-	}
-	jsonArray.push ({
-		"title": currentIcon + filename,
-		"arg": themeFilePath,
-		"subtitle": subtitlePrefix + "theme",
-		"type":"file:skipcheck",
-		"uid": themeFilePath,
+const snippetArr = app.doShellScript("find '" + snippetPath + "' -name '*.css' ").split("\r")
+	.map(snippetFilePath => {
+		const filename = snippetFilePath.replace(/.*\/(.*)\..+/, "$1");
+		return {
+			"title": filename,
+			"arg": snippetFilePath,
+			"subtitle": "snippet",
+			"type": "file:skipcheck",
+			"uid": snippetFilePath,
+		};
 	});
-});
-snippetArr.forEach(snippetFilePath => {
-	const filename = snippetFilePath.replace(/.*\/(.*)\..+/, "$1");
-	jsonArray.push ({
-		"title": filename,
-		"arg": snippetFilePath,
-		"subtitle": "snippet",
-		"type":"file:skipcheck",
-		"uid": snippetFilePath,
-	});
-});
 
-JSON.stringify({ "items": jsonArray });
-
+JSON.stringify({ "items": snippetArr });
