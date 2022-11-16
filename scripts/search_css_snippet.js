d4c4dc62cf0afc7f56ad7cbf6fb6c397976b9955
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-11-16 12:47:22 +0100

    better matcher for snippet search

--- scripts/search_css_snippet.js
+++ scripts/search_css_snippet.js
@@ -3,6 +3,14 @@ ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 
+function alfredMatcher(str) {
+	const clean = str.replace(/[-()_.:#]/g, " ");
+	const camelCaseSeperated = str.replace(/([A-Z])/g, " $1");
+	return [clean, camelCaseSeperated, str].join(" ");
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
 const homepath = app.pathTo("home folder");
 const vaultPath = $.getenv("vault_path").replace(/^~/, homepath);
 const snippetPath = vaultPath + "/.obsidian/snippets/";
@@ -13,6 +21,7 @@ const snippetArr = app.doShellScript("find '" + snippetPath + "' -name '*.css' "
 		const filename = snippetFilePath.replace(/.*\/(.*)\..+/, "$1");
 		return {
 			"title": filename,
+			"match": alfredMatcher(filename),
 			"arg": snippetFilePath,
 			"subtitle": "snippet",
 			"type": "file:skipcheck",
