9bba725fc4ee765cfeefa4f0dcfe37c0a41cce9a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-05 12:59:48 +0200

    Delete oversion.sh

--- scripts/oversion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-ObsidianVersion="$(ls ~"/Library/Application Support/obsidian/"*".asar" | egrep -o "\d+\.\d+\.\d+\.asar" | sed "s/\.asar//")"
-MacVersion="$(sw_vers -productVersion)"
-echo -n "macOS $MacVersion\nObsidian $ObsidianVersion"
