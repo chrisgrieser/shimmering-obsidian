b3d2fafd13f7a26c487058815a5dcc5a67ad995a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-18 22:27:04 +0200

    refactor!: starred to bookmarks, attachment search

--- scripts/attachment-search.js
+++ scripts/attachment-search.js
@@ -2,92 +2,100 @@
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str + " ";
 
-function readFile (path, encoding) {
-	if (!encoding) encoding = $.NSUTF8StringEncoding;
+const alfredMatcher = (/** @type {string} */ str) => str.replace(/[-()_.]/g, " ") + " " + str + " ";
+
+/** @param {string} path */
+function readFile(path) {
 	const fm = $.NSFileManager.defaultManager;
 	const data = fm.contentsAtPath(path);
-	const str = $.NSString.alloc.initWithDataEncoding(data, encoding);
+	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
 	return ObjC.unwrap(str);
 }
 
-function parentFolder (filePath) {
+/** @param {string} filePath */
+function parentFolder(filePath) {
 	if (!filePath.includes("/")) return "/";
-	return filePath.split("/").slice(0, -1)
-		.join("/");
+	return filePath.split("/").slice(0, -1).join("/");
 }
 
-//------------------------------------------------------------------------------
+//──────────────────────────────────────────────────────────────────────────────
 
 function getVaultPath() {
 	const theApp = Application.currentApplication();
 	theApp.includeStandardAdditions = true;
-	const dataFile = $.NSFileManager.defaultManager.contentsAtPath($.getenv("alfred_workflow_data") + "/vaultPath");
+	const dataFile = $.NSFileManager.defaultManager.contentsAtPath(
+		$.getenv("alfred_workflow_data") + "/vaultPath",
+	);
 	const vault = $.NSString.alloc.initWithDataEncoding(dataFile, $.NSUTF8StringEncoding);
 	return ObjC.unwrap(vault).replace(/^~/, theApp.pathTo("home folder"));
 }
-const vaultPath = getVaultPath()
+
+//──────────────────────────────────────────────────────────────────────────────
+
+const vaultPath = getVaultPath();
 const attachmentMetadata = vaultPath + "/.obsidian/plugins/metadata-extractor/allExceptMd.json";
 
 // filter the metadataJSON for the items w/ relativePaths of starred files
-const attachmentArr = JSON.parse(readFile(attachmentMetadata))
-	.nonMdFiles
-	.map(file => { /* eslint-disable-line complexity */
-		const filename = file.name;
-		const ext = file.name.split(".").pop();
-		const relativePath = file.relativePath;
-		const absolutePath = vaultPath + "/" + relativePath;
+const attachmentArr = JSON.parse(readFile(attachmentMetadata)).nonMdFiles.map((/** @type {{ name: string; relativePath: any; }} */ file) => {
+	/* eslint-disable-line complexity */ const filename = file.name;
+	const ext = file.name.split(".").pop();
+	const relativePath = file.relativePath;
+	const absolutePath = vaultPath + "/" + relativePath;
 
-		let emoji = "";
-		switch (ext) {
-			case "jpg":
-			case "jpeg":
-			case "png":
-			case "tiff":
-				emoji = "🏞";
-				break;
-			case "pdf":
-				emoji = "📕";
-				break;
-			case "wav":
-			case "mp3":
-				emoji = "🎙";
-				break;
-			case "mov":
-			case "mp4":
-				emoji = "🎞";
-				break;
-			case "js":
-			case "ts":
-			case "css":
-				emoji = "🧑‍💻";
-				break;
-			case "csv":
-			case "xlsx":
-				emoji = "📊";
-				break;
-			case "pptx":
-				emoji = "📙";
-				break;
-			case "docx":
-				emoji = "📘";
-				break;
-			default:
-				emoji = "📎";
-		}
+	let emoji = "";
+	switch (ext) {
+		case "jpg":
+		case "jpeg":
+		case "png":
+		case "tiff":
+			emoji = "🏞";
+			break;
+		case "pdf":
+			emoji = "📕";
+			break;
+		case "wav":
+		case "mp3":
+			emoji = "🎙";
+			break;
+		case "mov":
+		case "mp4":
+			emoji = "🎞";
+			break;
+		case "js":
+		case "ts":
+		case "css":
+			emoji = "🧑‍💻";
+			break;
+		case "csv":
+		case "xlsx":
+			emoji = "📊";
+			break;
+		case "pptx":
+			emoji = "📙";
+			break;
+		case "docx":
+			emoji = "📘";
+			break;
+		default:
+			emoji = "📎";
+	}
 
-		return {
-			"title": `${emoji} ${filename}`,
-			"match": alfredMatcher(filename),
-			"subtitle": "▸ " + parentFolder(relativePath),
-			"arg": relativePath,
-			"quicklookurl": vaultPath + "/" + relativePath,
-			"type": "file:skipcheck",
-			"uid": relativePath,
-			"mods": { "fn": { "arg": absolutePath } },
-		};
-
-	});
+	return {
+		title: `${emoji} ${filename}`,
+		match: alfredMatcher(filename),
+		subtitle: "▸ " + parentFolder(relativePath),
+		arg: absolutePath,
+		quicklookurl: absolutePath,
+		type: "file:skipcheck",
+		uid: absolutePath,
+		mods: {
+			shift: { arg: relativePath },
+			cmd: { arg: relativePath },
+			alt: { arg: relativePath },
+			ctrl: { arg: relativePath },
+		},
+	};
+});
 
 JSON.stringify({ items: attachmentArr });
--- scripts/bookmark-search.js
+++ scripts/bookmark-search.js
@@ -5,16 +5,15 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 const externalLinkRegex = /\[[^\]]*\]\([^)]+\)/;
 
-function parentFolder (filePath) {
+function parentFolder(filePath) {
 	if (!filePath.includes("/")) return "/";
-	return filePath.split("/").slice(0, -1)
-		.join("/");
+	return filePath.split("/").slice(0, -1).join("/");
 }
 
-const alfredMatcher = (str) => str.replace (/[-()_.]/g, " ") + " " + str;
+const alfredMatcher = (str) => str.replace(/[-()_.]/g, " ") + " " + str;
 const fileExists = (filePath) => Application("Finder").exists(Path(filePath));
 
-function readFile (path) {
+function readFile(path) {
 	const data = $.NSFileManager.defaultManager.contentsAtPath(path);
 	const str = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
 	return ObjC.unwrap(str);
@@ -25,7 +24,9 @@ function readFile (path) {
 function getVaultPath() {
 	const theApp = Application.currentApplication();
 	theApp.includeStandardAdditions = true;
-	const dataFile = $.NSFileManager.defaultManager.contentsAtPath($.getenv("alfred_workflow_data") + "/vaultPath");
+	const dataFile = $.NSFileManager.defaultManager.contentsAtPath(
+		$.getenv("alfred_workflow_data") + "/vaultPath",
+	);
 	const vault = $.NSString.alloc.initWithDataEncoding(dataFile, $.NSUTF8StringEncoding);
 	return ObjC.unwrap(vault).replace(/^~/, theApp.pathTo("home folder"));
 }
@@ -47,12 +48,12 @@ let stars = [];
 const bookmarks = [];
 if (fileExists(starredJSON)) {
 	stars = JSON.parse(readFile(starredJSON))
-		.items.filter(item => item.type === "file")
-		.map(item => item.path);
+		.items.filter((item) => item.type === "file")
+		.map((item) => item.path);
 }
 
 function bmFlatten(input, collector) {
-	input.forEach(item => {
+	input.forEach((item) => {
 		if (item.type === "file") collector.push(item.path);
 		if (item.type === "group") bmFlatten(item.items, collector);
 	});
@@ -62,7 +63,7 @@ if (fileExists(bookmarkJSON)) {
 	const bookm = JSON.parse(readFile(bookmarkJSON)).items;
 	bmFlatten(bookm, bookmarks);
 }
-const starsAndBookmarks = [...new Set([...stars, ...bookmarks])]
+const starsAndBookmarks = [...new Set([...stars, ...bookmarks])];
 console.log("starsAndBookmarks length:", starsAndBookmarks.length);
 
 //──────────────────────────────────────────────────────────────────────────────
@@ -71,7 +72,7 @@ let superIconList = [];
 if (superIconFile && fileExists(superIconFile)) {
 	superIconList = readFile(superIconFile)
 		.split("\n")
-		.filter(line => line.length !== 0);
+		.filter((line) => line.length !== 0);
 }
 console.log("superIconList length: " + superIconList.length);
 
@@ -79,10 +80,11 @@ const jsonArray = [];
 //──────────────────────────────────────────────────────────────────────────────
 
 // filter the metadataJSON for the items w/ relativePaths of starred files
-const fileArray = JSON.parse(readFile(metadataJSON))
-	.filter(item => starsAndBookmarks.includes(item.relativePath));
+const fileArray = JSON.parse(readFile(metadataJSON)).filter((item) =>
+	starsAndBookmarks.includes(item.relativePath),
+);
 
-fileArray.forEach(file => {
+fileArray.forEach((file) => {
 	const filename = file.fileName;
 	const relativePath = file.relativePath;
 	const absolutePath = vaultPath + "/" + relativePath;
@@ -95,12 +97,12 @@ fileArray.forEach(file => {
 		emoji += "🕑 ";
 		additionalMatcher += "recent ";
 	}
-	if (filename.toLowerCase().includes("kanban"))	iconpath = "icons/kanban.png";
+	if (filename.toLowerCase().includes("kanban")) iconpath = "icons/kanban.png";
 
 	let superchargedIcon = "";
 	let superchargedIcon2 = "";
 	if (superIconList.length > 0 && file.tags) {
-		superIconList.forEach(pair => {
+		superIconList.forEach((pair) => {
 			const tag = pair.split(",")[0].toLowerCase().replaceAll("#", "");
 			const icon = pair.split(",")[1];
 			const icon2 = pair.split(",")[2];
@@ -110,7 +112,7 @@ fileArray.forEach(file => {
 	}
 
 	// check link existence of file
-	let hasLinks = Boolean (file.links?.some(line => line.relativePath) || file.backlinks ); // no relativePath => unresolved link
+	let hasLinks = Boolean(file.links?.some((line) => line.relativePath) || file.backlinks); // no relativePath => unresolved link
 	if (!hasLinks) hasLinks = externalLinkRegex.test(readFile(absolutePath)); // readFile only executed when no other links found for performance
 	let linksSubtitle = "⛔️ Note without Outgoing Links or Backlinks";
 	if (hasLinks) linksSubtitle = "⇧: Browse Links in Note";
@@ -125,18 +127,18 @@ fileArray.forEach(file => {
 
 	// push result
 	jsonArray.push({
-		"title": emoji + superchargedIcon + displayName + superchargedIcon2,
-		"match": additionalMatcher + alfredMatcher(filename),
-		"subtitle": "▸ " + parentFolder(relativePath),
-		"arg": relativePath,
-		"quicklookurl": vaultPath + "/" + relativePath,
-		"type": "file:skipcheck",
-		"uid": relativePath,
-		"icon": { "path": iconpath },
-		"mods": {
-			"shift": {
-				"valid": hasLinks,
-				"subtitle": linksSubtitle,
+		title: emoji + superchargedIcon + displayName + superchargedIcon2,
+		match: additionalMatcher + alfredMatcher(filename),
+		subtitle: "▸ " + parentFolder(relativePath),
+		arg: relativePath,
+		quicklookurl: vaultPath + "/" + relativePath,
+		type: "file:skipcheck",
+		uid: relativePath,
+		icon: { path: iconpath },
+		mods: {
+			shift: {
+				valid: hasLinks,
+				subtitle: linksSubtitle,
 			},
 		},
 	});
