abcd50abfd9969bbd085fcc8d598f940fd160aef
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-01-21 13:57:43 +0100

    fix: note not opening when Obsidian isn't running

--- scripts/open-note.js
+++ scripts/open-note.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env osascript -l JavaScript
-
 ObjC.import("stdlib");
 const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-
 //──────────────────────────────────────────────────────────────────────────────
 
 /** @type {AlfredRun} */
@@ -11,8 +9,6 @@ app.includeStandardAdditions = true;
 function run(argv) {
 	const vaultPath = $.getenv("vault_path");
 	const vaultNameEnc = encodeURIComponent(vaultPath.replace(/.*\//, ""));
-
-	const obsiRunningAlready = Application("Obsidian").running();
 
 	const input = argv[0].trim(); // trim to remove trailing \n
 	const relativePath = input.split("#")[0].split(":")[0];
@@ -25,8 +21,16 @@ function run(argv) {
 	if (heading) urlScheme += "&heading=" + encodeURIComponent(heading);
 	else if (lineNum) urlScheme += "&line=" + encodeURIComponent(lineNum);
 
-	app.openLocation(urlScheme);
-
-	// press `Esc` to leave settings menu potentially open
-	if (obsiRunningAlready) Application("System Events").keyCode(53);
+	// open note
+	if (Application("Obsidian").running()) {
+		app.openLocation(urlScheme);
+		// press `Esc` to leave settings menu potentially open
+		Application("System Events").keyCode(53);
+	} else {
+		// delay opening URI scheme until Obsidian is running
+		Application("Obsidian").launch();
+		delay(1); // hard-coded, since Obsidian is registered as running before Advanced URI plugin loads
+		app.openLocation(urlScheme);
+	}
+	return null;
 }
