e7e8b79475157a0b20a7c4bd349b82c214d633bb
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-04-13 08:25:47 +0200

    chore: cleanup leftover files

--- scripts/make-link-discord-ready.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-url="$*"
-frontmost=$(osascript -e 'tell application "System Events" to get name of application processes whose frontmost is true and visible is true')
-
-if [[ "$frontmost" =~ Discord ]]; then
-	echo -n "<$url>"
-else
-	echo -n "$url"
-fi
