78d24c0a53094e7eb8c558e8fad2fd80ff7233b7
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-04-20 10:25:09 +0200

    chore: minor cleanup

--- scripts/o_search.js
+++ scripts/o_search.js
@@ -43,6 +43,10 @@ function run() {
 	const bookmarkJSON = `${vaultConfig}/bookmarks.json`;
 	const excludeFilterJSON = `${vaultConfig}/app.json`;
 	const superIconFile = $.getenv("supercharged_icon_file");
+	const removeEmojis = $.getenv("remove_emojis") === "1";
+	// exclude cssclass: private
+	const censorChar = $.getenv("censor_char");
+	const privacyModeOn = $.getenv("privacy_mode") === "1";
 
 	let recentJSON = `${vaultConfig}/workspace.json`;
 	if (!fileExists(recentJSON)) recentJSON = recentJSON.slice(0, -5); // Obsidian 0.16 uses workspace.json → https://discord.com/channels/686053708261228577/716028884885307432/1013906018578743478
@@ -170,10 +174,6 @@ function run() {
 		headingIgnore[i] = shouldIgnore;
 	}
 
-	// exclude cssclass: private
-	const censorChar = $.getenv("censor_char");
-	const privacyModeOn = $.getenv("privacy_mode") === "1";
-
 	//──────────────────────────────────────────────────────────────────────────────
 	// CONSTRUCTION OF JSON FOR ALFRED
 	const resultsArr = [];
@@ -202,7 +202,7 @@ function run() {
 		if (isBookmarked) emoji += "🔖 ";
 		if (isRecent) emoji += "🕑 ";
 		if (filename.toLowerCase().includes("kanban")) iconpath = "icons/kanban.png";
-		if ($.getenv("remove_emojis") === "1") emoji = "";
+		if (removeEmojis) emoji = "";
 
 		let superchargedIcon = "";
 		let superchargedIcon2 = "";
@@ -269,9 +269,7 @@ function run() {
 				icon: { path: headingIconpath },
 				mods: {
 					alt: { arg: relativePath },
-					shift: {
-						arg: relativePath,
-					},
+					shift: { arg: relativePath },
 				},
 			});
 		}
@@ -281,7 +279,6 @@ function run() {
 	for (const canvas of canvasArray) {
 		const name = canvas.basename;
 		const relativePath = canvas.relativePath;
-		const denyForCanvas = { valid: false, subtitle: "⛔ Cannot do that with a canvas." };
 		const isBookmarked = starsAndBookmarks.includes(relativePath);
 		const isRecent = recentFiles.includes(relativePath);
 
@@ -303,8 +300,8 @@ function run() {
 			icon: { path: "icons/canvas.png" },
 			uid: relativePath,
 			mods: {
-				shift: denyForCanvas,
-				fn: denyForCanvas,
+				shift: { valid: false, subtitle: "⛔ Cannot do that with a canvas." },
+				fn: { valid: false, subtitle: "⛔ Cannot do that with a canvas." },
 			},
 		});
 	}
@@ -363,9 +360,6 @@ function run() {
 
 	return JSON.stringify({
 		items: resultsArr,
-		cache: {
-			seconds: 600,
-			loosereload: true,
-		},
+		cache: { seconds: 600, loosereload: true },
 	});
 }
