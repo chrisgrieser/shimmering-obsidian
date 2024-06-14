7dcd3843cd86fa9062294600ec5d791756bcd7c6
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-14 11:10:30 +0200

    build: update build-and-release script

--- .build-and-release.sh
+++ .build-and-release.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-# BUILD AND RELEASE
 #───────────────────────────────────────────────────────────────────────────────
 
 # goto git root
@@ -12,30 +11,37 @@ echo -n "   next version: "
 read -r nextVersion
 echo "────────────────────────"
 
-# update version number in *repo* info.plist
+# GUARD
+if [[ -z "$nextVersion" || "$nextVersion" == "$currentVersion" ]]; then
+	print "\033[1;31mInvalid version number.\033[0m"
+	exit 1
+fi
+
+# update version number in THE REPO'S `info.plist`
 plutil -replace version -string "$nextVersion" info.plist
 
-# #───────────────────────────────────────────────────────────────────────────────
-#
-# INFO specific to my setup: update version number in *local* info.plist 
-localInfoPlist="$HOME/.config/Alfred.alfredpreferences/workflows/$(basename "$PWD")/info.plist"
+#───────────────────────────────────────────────────────────────────────────────
+# INFO
+# this section assumes the local folder is named the same as the github repo
+
+# update version number in LOCAL `info.plist`
+prefs_location=$(grep "current" "$HOME/Library/Application Support/Alfred/prefs.json" | cut -d'"' -f4 | sed -e 's|\\/|/|g' -e "s|^~|$HOME|")
+workflow_name="$(basename "$PWD")"
+localInfoPlist="$HOME/.config/Alfred.alfredpreferences/workflows/$workflow_name/info.plist"
 if [[ -f "$localInfoPlist" ]]; then
 	plutil -replace version -string "$nextVersion" "$localInfoPlist"
 fi
 
 # convenience: copy download link for current version
-workflow_name="$(basename "$PWD")"
-echo -n "https://github.com/chrisgrieser/${workflow_name}/releases/download/${nextVersion}/${workflow_name}.alfredworkflow" |
+github_username="chrisgrieser"
+echo -n "https://github.com/$github_username/$workflow_name/releases/download/$nextVersion/${workflow_name}.alfredworkflow" |
 	pbcopy
 
 #───────────────────────────────────────────────────────────────────────────────
 
-# git operations
-git add -A
-git commit -m "release: $nextVersion"
-git pull
-git push
-
-# trigger the release action via github action
-git tag "$nextVersion" 
-git push origin --tags
+git add --all &&
+	git commit -m "release: $nextVersion" &&
+	git pull &&
+	git push &&
+	git tag "$nextVersion" &&
+	git push origin --tags # trigger the GitHub release action
