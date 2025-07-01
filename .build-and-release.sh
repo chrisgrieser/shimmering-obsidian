bc9ecbf877a132cd3c323cc589465d152ca9a4b4
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-07-01 14:42:49 +0200

    ci: update for Alfred Gallery update submission changes

--- .build-and-release.sh
+++ .build-and-release.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
-#───────────────────────────────────────────────────────────────────────────────
 
 # goto git root
 cd "$(git rev-parse --show-toplevel)" || return 1
+
+#───────────────────────────────────────────────────────────────────────────────
 
 # Prompt for next version number
 current_version=$(plutil -extract version xml1 -o - info.plist | sed -n 's/.*<string>\(.*\)<\/string>.*/\1/p')
@@ -13,34 +14,55 @@ echo "────────────────────────"
 
 # GUARD
 if [[ -z "$next_version" || "$next_version" == "$current_version" ]]; then
-	print "\033[1;31mInvalid version number.\033[0m"
+	print "\e[1;31mInvalid version number.\e[0m"
 	return 1
 fi
 
-# update version number in THE REPO'S `info.plist`
+#───────────────────────────────────────────────────────────────────────────────
+# update version number in THE REPO's `info.plist`
 plutil -replace version -string "$next_version" info.plist
 
-#───────────────────────────────────────────────────────────────────────────────
+# update version number in LOCAL `info.plist`
 # INFO this assumes the local folder is named the same as the github repo
-# 1. update version number in LOCAL `info.plist`
-# 2. convenience: copy download link for current version
 
 # update version number in LOCAL `info.plist`
 prefs_location=$(defaults read com.runningwithcrayons.Alfred-Preferences syncfolder | sed "s|^~|$HOME|")
 workflow_uid="$(basename "$PWD")"
 local_info_plist="$prefs_location/Alfred.alfredpreferences/workflows/$workflow_uid/info.plist"
-if [[ -f "$local_info_plist" ]] ; then 
+if [[ -f "$local_info_plist" ]]; then
 	plutil -replace version -string "$next_version" "$local_info_plist"
 else
-	print "\033[1;33mCould not increment version, local \`info.plist\` not found: '$local_info_plist'\033[0m"
+	print "\e[1;33mCould not increment version, local \`info.plist\` not found: '$local_info_plist'\e[0m"
 	return 1
 fi
 
-# copy download link for current version
+#───────────────────────────────────────────────────────────────────────────────
+
+# copy download link for current version, to share it when closing issues
 msg="Available in the Alfred Gallery in 1-2 days, or directly by downloading the latest release here:"
-github_user=$(git remote --verbose | head -n1 | sed -E 's/.*github.com[:\](.*)\/.*/\1/')
-url="https://github.com/$github_user/$workflow_uid/releases/download/$next_version/${workflow_uid}.alfredworkflow"
+repo=$(git remote --verbose | head -n1 | sed -E 's/.*github.com:([^[:space:]]*).*/\1/')
+url="https://github.com/$repo/releases/download/$next_version/$workflow_uid.alfredworkflow"
 echo -n "$msg $url" | pbcopy
+
+#───────────────────────────────────────────────────────────────────────────────
+
+# Submit update at Alfred Gallery
+last_release_commit=$(git log --grep="^release: " -n1 --pretty=format:"%H")
+if [[ -z "$last_release_commit" ]]; then
+	root_commit=$(git rev-list --max-parents=0 HEAD)
+	last_release_commit=$root_commit
+fi
+changelog=$(git log "$last_release_commit"..HEAD --format='- %s' |
+	grep --extended-regexp --invert-match '^- (build|ci|release|chore|test)' |
+	sed -E "s/^- ([^ ]+): /- **\1**: /")
+if [[ -n "$changelog" ]]; then
+	repo=$(git remote --verbose | head -n1 | sed -E 's/.*github.com:([^[:space:]]*).*/\1/')
+	gallery_url="https://github.com/$repo" # github-url also okay for Alfred devs
+	open "https://github.com/alfredapp/gallery-edits/issues/new?template=02_update_workflow.yml&title=Update+Workflow:+$repo&gallery_url=$gallery_url&new_version=$next_version&changelog=$changelog"
+else
+	echo "Only internal changes, thus not submitting update at Alfred Gallery."
+	echo
+fi
 
 #───────────────────────────────────────────────────────────────────────────────
 
